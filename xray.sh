39c6cdfdea249b5b61279b0954fcfba2e0c406f4
got0707f <108911728+got0707f@users.noreply.github.com>
2022-10-09 23:35:33 +0000

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 if [ ! -f UUID ]; then
-  UUID="99996194-5d76-4b5f-9da7-3830f0d8f9b7"
-fi
-if [ ! -f UUID ]; then
-  UUID="fa5d55e7-ea8f-4ddd-b39d-7dd4b3a2f03b"
+   UUID="fa5d55e7-ea8f-4ddd-b39d-7dd4b3a2f03b"
 fi
 
 # Set config.json
